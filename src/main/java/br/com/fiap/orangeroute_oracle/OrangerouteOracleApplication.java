67a32d7434118eb22720af2ce14328b792cfe645
JhowQT <jhon.q.torrez@gmail.com>
2025-10-30 09:26:38 -0300

    criação do respositorio para o novo projeto

---
+++ src/main/java/br/com/fiap/orangeroute_oracle/OrangerouteOracleApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.orangeroute_oracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangerouteOracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangerouteOracleApplication.class, args);
+	}
+
+}
