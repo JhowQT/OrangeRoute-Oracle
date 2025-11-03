7716ce73856fc9ffcbefdac77e9ba37658242638
JhowQT <jhon.q.torrez@gmail.com>
2025-11-03 10:13:57 -0300

    Criação das classes entitys, bases para estrutura no back-end

---
+++ src/main/java/br/com/fiap/orangeroute_oracle/entity/Usuario.java
@@ -0,0 +1,37 @@
+package br.com.fiap.orangeroute_oracle.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "T_OR_USUARIO")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
+    private Long idUsuario;
+
+    @Column(name = "nm_usuario", nullable = false, length = 100)
+    private String nomeUsuario;
+
+    @Column(name = "email", nullable = false, unique = true, length = 150)
+    private String email;
+
+    @Column(name = "senha", nullable = false, length = 150)
+    private String senha;
+
+    @Lob
+    @Column(name = "foto")
+    private byte[] foto;
+
+    @Column(name = "at_usuario", length = 1)
+    private String ativo;
+
+    @ManyToOne
+    @JoinColumn(name = "id_tipo_usuario", nullable = false)
+    private TipoUsuario tipoUsuario;
+}
