35b542169a43fcbf4a1a3b73a1291093f3571b71
JhowQT <jhon.q.torrez@gmail.com>
2025-11-07 15:05:27 -0300

    finalização da criação das classes DAO

---
+++ src/main/java/br/com/fiap/orangeroute_oracle/oracledao/ComentarioDAO.java
@@ -0,0 +1,68 @@
+package br.com.fiap.orangeroute_oracle.oracledao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Component
+public class ComentarioDAO {
+
+    @Autowired
+    private DataSource dataSource;
+
+    public void inserirComentario(String conteudo, String ativo, int idUsuario, int idTrilhaCarreira) {
+        String procedure = "{call prc_insere_comentario(?, ?, ?, ?)}";
+        try (Connection conn = dataSource.getConnection();
+             CallableStatement stmt = conn.prepareCall(procedure)) {
+
+            stmt.setString(1, conteudo);
+            stmt.setString(2, ativo);
+            stmt.setInt(3, idUsuario);
+            stmt.setInt(4, idTrilhaCarreira);
+            stmt.execute();
+
+            System.out.println("INSERT executado (Comentário): Usuário " + idUsuario + " → Trilha " + idTrilhaCarreira);
+
+        } catch (SQLException e) {
+            System.err.println("Erro INSERT (Comentário): " + e.getMessage());
+        }
+    }
+
+    public void atualizarComentario(int idComentario, String conteudo, String ativo, int idUsuario, int idTrilhaCarreira) {
+        String procedure = "{call prc_update_comentario(?, ?, ?, ?, ?)}";
+        try (Connection conn = dataSource.getConnection();
+             CallableStatement stmt = conn.prepareCall(procedure)) {
+
+            stmt.setInt(1, idComentario);
+            stmt.setString(2, conteudo);
+            stmt.setString(3, ativo);
+            stmt.setInt(4, idUsuario);
+            stmt.setInt(5, idTrilhaCarreira);
+            stmt.execute();
+
+            System.out.println("UPDATE executado (Comentário ID: " + idComentario + ")");
+
+        } catch (SQLException e) {
+            System.err.println("Erro UPDATE (Comentário): " + e.getMessage());
+        }
+    }
+
+    public void deletarComentario(int idComentario) {
+        String procedure = "{call prc_delete_comentario(?)}";
+        try (Connection conn = dataSource.getConnection();
+             CallableStatement stmt = conn.prepareCall(procedure)) {
+
+            stmt.setInt(1, idComentario);
+            stmt.execute();
+
+            System.out.println("DELETE executado (Comentário ID: " + idComentario + ")");
+
+        } catch (SQLException e) {
+            System.err.println("Erro DELETE (Comentário): " + e.getMessage());
+        }
+    }
+}
