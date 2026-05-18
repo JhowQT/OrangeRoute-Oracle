339ae391a9bcc62a5db02324ebb91d6a62e66ee1
JhowQT <jhon.q.torrez@gmail.com>
2026-05-18 20:48:03 -0300

    implementando as mudanças

--- src/main/java/br/com/fiap/orangeroute_oracle/config/RsaKeyConfig.java
+++ src/main/java/br/com/fiap/orangeroute_oracle/config/RsaKeyConfig.java
@@ -2,9 +2,9 @@ package br.com.fiap.orangeroute_oracle.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.InputStream;
 import java.security.KeyFactory;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
@@ -16,31 +16,41 @@ public class RsaKeyConfig {
 
     @Bean
     public RSAPublicKey publicKey() throws Exception {
-        String key = new String(Files.readAllBytes(
-                Paths.get("src/main/resources/certs/public-key.pem")));
+
+        InputStream inputStream =
+                new ClassPathResource("certs/public-key.pem").getInputStream();
+
+        String key = new String(inputStream.readAllBytes());
 
         key = key.replace("-----BEGIN PUBLIC KEY-----", "")
-                 .replace("-----END PUBLIC KEY-----", "")
-                 .replaceAll("\\s", "");
+                .replace("-----END PUBLIC KEY-----", "")
+                .replaceAll("\\s", "");
 
         byte[] decoded = Base64.getDecoder().decode(key);
 
         X509EncodedKeySpec spec = new X509EncodedKeySpec(decoded);
-        return (RSAPublicKey) KeyFactory.getInstance("RSA").generatePublic(spec);
+
+        return (RSAPublicKey)
+                KeyFactory.getInstance("RSA").generatePublic(spec);
     }
 
     @Bean
     public RSAPrivateKey privateKey() throws Exception {
-        String key = new String(Files.readAllBytes(
-                Paths.get("src/main/resources/certs/private-key.pem")));
+
+        InputStream inputStream =
+                new ClassPathResource("certs/private-key.pem").getInputStream();
+
+        String key = new String(inputStream.readAllBytes());
 
         key = key.replace("-----BEGIN PRIVATE KEY-----", "")
-                 .replace("-----END PRIVATE KEY-----", "")
-                 .replaceAll("\\s", "");
+                .replace("-----END PRIVATE KEY-----", "")
+                .replaceAll("\\s", "");
 
         byte[] decoded = Base64.getDecoder().decode(key);
 
         PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(decoded);
-        return (RSAPrivateKey) KeyFactory.getInstance("RSA").generatePrivate(spec);
+
+        return (RSAPrivateKey)
+                KeyFactory.getInstance("RSA").generatePrivate(spec);
     }
 }
